240c3c230efcf3998eeb36e3ac378f639687c077
Solo Coder <kisliydanecka@gmail.com>
2024-02-20 02:31:00 +0300

    three complete

---
+++ sum_and_sub.py
@@ -0,0 +1,8 @@
+def sum_and_sub(a, b):
+    sum_result = a + b
+    sub_result = a - b
+    return sum_result, sub_result
+
+sum_result, sub_result = sum_and_sub(10.5,  2234.2)
+print("Сумма:", sum_result)
+print("Разность:", sub_result)
