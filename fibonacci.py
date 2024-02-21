dd4773caecea3c10c1d96e997356ab772931f826
Solo Coder <kisliydanecka@gmail.com>
2024-02-21 13:33:19 +0300

    complete

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,11 @@
-cube = lambda x: # complete the lambda function 
+cube = lambda x: x **  3
 
 def fibonacci(n):
-    # return a list of fibonacci numbers
+    fib_numbers = [0,  1]
+    for i in range(2, n):
+        fib_numbers.append(fib_numbers[i-1] + fib_numbers[i-2])
+    return fib_numbers
 
-    if __name__ == '__main__':
-        n = int(input())
-        print(list(map(cube, fibonacci(n))))
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
